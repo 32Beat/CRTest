11ca2bfd2e05aac0a09318e5d963150a18980c8f
32Beat <opgr@32bts-macbook-pro.local>
2016-03-01 12:13:49 +0100

    Initial Commit

---
+++ CRTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CRTest
+//
+//  Created by 32BT on 01/03/16.
+//  Copyright © 2016 32BT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CRTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CRTest
+//
+//  Created by 32BT on 01/03/16.
+//  Copyright © 2016 32BT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
